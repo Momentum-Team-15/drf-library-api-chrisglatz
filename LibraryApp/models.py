38acc3c8ac2a81baf4ca59e4dd5fa3147d3ba082
Chris Glatz <cg.glatz@gmail.com>
2022-11-08 22:00:34 -0500

    added URL and view

--- LibraryApp/models.py
+++ LibraryApp/models.py
@@ -6,3 +6,13 @@ class User(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+
+class Books(models.Model)
+    title = models.CharField(max_length=200)
+    author = models.Charfield(max_length=50)
+    genre = models.Charfield(max_length=50)
+    pubdate = models.datefield()
+    is_favorite = models.BooleanField()
+
+    def __str__(self):
+        return self.title
---
+++ LibraryApp/urls.py
@@ -0,0 +1,6 @@
+from djano.urls import path
+from . import views 
+
+urlpatterns = [
+    path('', views.book_list, name='book_list')
+]
